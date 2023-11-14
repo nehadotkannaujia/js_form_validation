da9e62106d625c58d5e18cdc3436ab25e6d74a88
nehadotkannaujia <neha.kannaujia@gmail.com>
2023-11-14 11:22:33 +0000

    finished form validation

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ const formProcessing = (formSelector) => {
 
    const validateOptions = [
     {
+        attribute : "pattern",
+        isValid : (input) => {
+           const patternRegex = new RegExp(input.pattern)
+          return patternRegex.test(input.value)
+        },
+        errorMessage : label => `${label.textContent} should be valid`
+    },
+    {
         attribute : "required",
         isValid : (input) => input.value.trim() !== "",
         errorMessage : label => `${label.textContent} is mandatory`
@@ -12,20 +20,34 @@ const formProcessing = (formSelector) => {
 
    const validateSingleFormGroup = formGroup => {
     const label = formGroup.querySelector("label");
-    const input = formGroup.querySelector("input")
-    const error = formGroup.querySelector(".error")
+    const input = formGroup.querySelector("input");
+    const error = formGroup.querySelector(".error-text");
+    const errorIcon = formGroup.querySelector(".warning-icon");
+    const validIcon = formGroup.querySelector(".valid-icon");
     let formError = false;
 
     for(const option of validateOptions){
 
         if(input.hasAttribute(option.attribute) && !option.isValid(input)){
-            formError = true
-            error.textContent = option.errorMessage(label)
+            formError = true;
+            error.textContent = option.errorMessage(label);
+            errorIcon.classList.add("show");
+            errorIcon.classList.remove("hide");
+            input.classList.add("red-border");
+            input.classList.remove("green-border");
+            validIcon.classList.add("hide");
+            validIcon.classList.remove("show");
         }
 
     }
     if(!formError){
         error.textContent = null;
+        input.classList.remove("red-border");
+        input.classList.add("green-border");
+        errorIcon.classList.add("hide");
+        errorIcon.classList.remove("show");
+        validIcon.classList.add("show");
+        validIcon.classList.remove("hide");
     }
 
    }
